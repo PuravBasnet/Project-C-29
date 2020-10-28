7a9640750aa60717f2cf6a7cbdebabe140e742a7
PuravBasnet <71262229+PuravBasnet@users.noreply.github.com>
2020-10-28 19:20:32 +0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,104 @@
+const Bodies=Matter.Bodies;
+const Engine=Matter.Engine;
+const World=Matter.World;
+var engine,world;
+
+function setup() {
+  createCanvas(900,400);
+  engine=Engine.create();
+  world=engine.world;
+
+  block1=new Block(300,275,30,40);
+  block2=new Block(330,275,30,40);
+  block3=new Block(360,275,30,40);
+  block4=new Block(390,275,30,40);
+  block5=new Block(420,275,30,40);
+  block6=new Block(450,275,30,40);
+  block7=new Block(480,275,30,40);
+
+  block8=new Block(330,235,30,40);
+  block9=new Block(360,235,30,40);
+  block10=new Block(390,235,30,40);
+  block11=new Block(420,235,30,40);
+  block12=new Block(450,235,30,40);
+
+  block13=new Block(360,195,30,40);
+  block14=new Block(390,195,30,40);
+  block15=new Block(420,195,30,40);
+
+  block16=new Block(390,155,30,40);
+
+  block17=new Block(390,355,30,40);
+  block18=new Block(390,355,30,40);
+  block19=new Block(390,355,30,40);
+  block10=new Block(390,355,30,40);
+  block21=new Block(390,355,30,40);
+  block22=new Block(390,355,30,40);
+  block23=new Block(390,355,30,40);
+
+  block24=new Block(390,155,30,40);
+  block25=new Block(390,155,30,40);
+  block26=new Block(390,155,30,40);
+  block27=new Block(390,155,30,40);
+  block28=new Block(390,155,30,40);
+  
+  block29=new Block(390,155,30,40);
+  block30=new Block(390,155,30,40);
+  block31=new Block(390,155,30,40);
+ 
+  block32=new Block(390,155,30,40);
+
+  ground = new Ground(450,390,900,20);
+  stand1 = new Ground(390,300,250,10);
+  stand2 = new Ground(700,200,200,10);
+}
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+
+  ground.display();
+
+  stand1.display();
+  stand2.display();
+
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+
+  block13.display();
+  block14.display();
+  block15.display();
+
+  block16.display();
+
+  block17.display();
+  block18.display();
+  block19.display();
+  block20.display();
+  block21.display();
+  block22.display();
+  block23.display();
+
+  block24.display();
+  block25.display();
+  block26.display();
+  block27.display();
+  block28.display();
+
+  block29.display();
+  block30.display();
+  block31.display();
+
+  block32.display();
+
+}
